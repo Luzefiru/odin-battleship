2921a447d1e3316410c3d8e2d3b7ca8a3220dceb
Luzefiru <ian.deejay.iv@gmail.com>
2023-03-19 16:23:00 +0800

    feat: add dynamic rendering via viewModule

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { Ship } from './ship';
 import { Gameboard } from './gameboard';
 import { Player, Computer } from './player';
+import { viewModule } from './view';
 
 // setup player's board
 const player = Player('TestPlayer');
@@ -10,8 +11,6 @@ player.deployShip(3, 1, 2);
 player.deployShip(4, 4, 2);
 player.deployShip(5, 6, 2);
 
-console.log(player.getBoard().getGrid());
-
 // setup computer's board
 const computer = Computer();
 computer.deployShip(1, 0, 0);
@@ -20,4 +19,4 @@ computer.deployShip(3, 0, 3);
 computer.deployShip(4, 1, 0);
 computer.deployShip(5, 6, 2);
 
-console.log(computer.getBoard().getGrid());
+viewModule.renderBoards(player, computer);
---
+++ src/view.js
@@ -0,0 +1,55 @@
+const viewModule = (function () {
+  const renderBoards = (player, enemy) => {
+    const boardOfPlayer = document.querySelector('.grid--player');
+    const boardOfEnemy = document.querySelector('.grid--enemy');
+
+    let rowCtr;
+    let cellCtr;
+    // renders player's board
+    rowCtr = 0;
+    cellCtr = 0;
+    for (let row of player.getBoard().getGrid()) {
+      console.log(row);
+      for (let cell of row) {
+        const newCell = document.createElement('div');
+        newCell.classList.add('grid--player__cell', 'cell');
+
+        if (player.getBoard().getShotsGrid()[rowCtr][cellCtr] === 1) {
+          if (cell !== 'miss') {
+            newCell.classList.add('confirm');
+          }
+          newCell.classList.add('hit');
+        }
+
+        boardOfPlayer.append(newCell);
+        cellCtr++;
+      }
+      rowCtr++;
+    }
+
+    // renders enemy's board
+    rowCtr = 0;
+    cellCtr = 0;
+    for (let row of enemy.getBoard().getGrid()) {
+      for (let cell of row) {
+        const newCell = document.createElement('div');
+        newCell.classList.add('grid--enemy_cell', 'cell');
+
+        if (enemy.getBoard().getShotsGrid()[rowCtr][cellCtr] === 1) {
+          if (cell !== 'miss') {
+            newCell.classList.add('confirm');
+          }
+          newCell.classList.add('hit');
+        }
+
+        boardOfEnemy.append(newCell);
+        cellCtr++;
+      }
+      rowCtr++;
+    }
+  };
+
+  return { renderBoards };
+})();
+
+export { viewModule };
